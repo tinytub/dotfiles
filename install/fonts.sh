0760a67c4c2974d29efe054b853dacd339e88842
tinytub <zhaopeng1988@mail.com>
2020-10-14 14:58:35 +0800

    add font patch script

---
+++ install/fonts.sh
@@ -0,0 +1,31 @@
+#  brew cask install font-inconsolata-go-nerd-font
+#  brew cask install font-hack-nerd-font font-meslo-nerd-font font-sourcecodepro-nerd-font
+#  brew install font-meslo-nerd-font
+
+# script from https://gist.github.com/ELLIOTTCABLE/5b87ab21b11acb76a5c52d47a022b519
+
+# Creating this because I'm *sure* I'll forget how to do this.
+
+# 1. Customize your Input fontface, and download it from their website:
+open -a Safari \
+   "http://input.fontbureau.com/download/index.html?size=14&language=javascript&theme=base16-dark&family=InputMono&width=200&weight=300&line-height=1.2&a=0&g=ss&i=serif&l=serifs_round&zero=slash&asterisk=height&braces=straight&preset=dejavu&customize=please"
+
+# 2. Download the ‘patcher script’:
+# (I have no idea why the hell this script requires the `changelog.md` as well; and we [ab]use
+#  GitHub's SVN bridge to download *just* the `src/glyphs`, instead of the 100s of megabytes of
+#  pre-patched fonts)
+cd ~/Downloads/Input-Font
+curl -o nerd-patcher.py -JO -fsSl --proto-redir -all,https \
+   https://raw.githubusercontent.com/ryanoasis/nerd-fonts/v2.1.0/{font-patcher,changelog.md}
+svn checkout https://github.com/ryanoasis/nerd-fonts/branches/v2.1.0/src/glyphs src/glyphs
+
+# 3. Install the patcher-script's dependencies:
+brew install fontforge
+
+# 4. Patch the files:
+for font in Input_Fonts/Input/*.ttf; do
+   #python nerd-patcher.py --careful --complete --progressbars "$font"; done
+   fontforge -script ./font-patcher --careful --complete --progressbars --adjust-line-height "$font"; done
+
+# 5. Install the patched fonts:
+open -a 'Font Book' 'Input '*
