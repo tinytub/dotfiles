dc5027823730a256700a82e44b5d291b145f7851
tinytub <zhaopeng1988@mail.com>
2021-03-26 16:44:02 +0800

    update

--- config/nvim/lua/core/options.lua
+++ config/nvim/lua/core/options.lua
@@ -127,8 +127,11 @@ local function load_options()
       },
       cache_enabled = 0
     }
-    vim.g.python_host_prog = '/usr/bin/python'
-    vim.g.python3_host_prog = '/usr/local/bin/python3'
+
+    virtualenv = global.cache_dir..'/venv/bin/python'
+    --vim.g.python_host_prog = '/usr/bin/python'
+    --vim.g.python3_host_prog = '/usr/local/bin/python3'
+    vim.g.python3_host_prog = virtualenv
   end
   for name, value in pairs(global_local) do
     vim.o[name] = value
