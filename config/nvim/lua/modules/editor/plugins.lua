f0e058f59c7246ba8465195b444f95924ccb013b
tinytub <zhaopeng1988@mail.com>
2021-04-01 18:18:50 +0800

    update

--- config/nvim/lua/modules/editor/plugins.lua
+++ config/nvim/lua/modules/editor/plugins.lua
@@ -27,12 +27,26 @@ editor['itchyny/vim-cursorword'] = {
   config = conf.vim_cursorwod
 }
 
-editor['hrsh7th/vim-eft'] = {
-  opt = true,
-  config = function()
-    vim.g.eft_ignorecase = true
+editor['unblevable/quick-scope'] = {
+  config = function ()
+    -- Trigger a highlight in the appropriate direction when pressing these keys:
+    -- vim.cmd('let g:qs_highlight_on_keys = [\'f\', \'F\', \'t\', \'T\']')
+    vim.g.qs_highlight_on_keys = {'f', 'F', 't', 'T'}
+    vim.g.qs_max_chars=150
+    vim.api.nvim_command('augroup quickscopecolor')
+    vim.api.nvim_command('autocmd!')
+    vim.api.nvim_command("autocmd ColorScheme * highlight QuickScopePrimary guifg='#b1fa87' gui=underline ctermfg=155 cterm=underline")
+    vim.api.nvim_command("autocmd ColorScheme * highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline")
+    vim.api.nvim_command('augroup END')
   end
 }
+
+--editor['hrsh7th/vim-eft'] = {
+--  opt = true,
+--  config = function()
+--    vim.g.eft_ignorecase = true
+--  end
+--}
 
 editor['kana/vim-operator-replace'] = {
   keys = {{'x','p'}},
