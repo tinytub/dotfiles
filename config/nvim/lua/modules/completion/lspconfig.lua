b99c0db0c101004fbd3715d08b55542bdd0616db
tinytub <zhaopeng1988@mail.com>
2021-04-16 11:13:34 +0800

    update

--- config/nvim/lua/modules/completion/lspconfig.lua
+++ config/nvim/lua/modules/completion/lspconfig.lua
@@ -69,10 +69,55 @@ vim.cmd('command! -nargs=0 LspRestart call v:lua.reload_lsp()')
 --  texthl = "LspDiagnosticsDefaultHint",
 --  numhl = "LspDiagnosticsDefaultHint"
 --}})
-vim.fn.sign_define("LspDiagnosticsSignError",{ text = "", texthl = "LspDiagnosticsDefaultError" })
-vim.fn.sign_define("LspDiagnosticsSignWarning",{ text = "", texthl = "LspDiagnosticsDefaultWarning" })
-vim.fn.sign_define("LspDiagnosticsSignInformation",{ text = "", texthl = "LspDiagnosticsDefaultInformation" })
-vim.fn.sign_define("LspDiagnosticsSignHint",{ text = "", texthl = "LspDiagnosticsDefaultHint" })
+--vim.fn.sign_define("LspDiagnosticsSignError",{ text = "", texthl = "LspDiagnosticsDefaultError" })
+--vim.fn.sign_define("LspDiagnosticsSignWarning",{ text = "", texthl = "LspDiagnosticsDefaultWarning" })
+--vim.fn.sign_define("LspDiagnosticsSignInformation",{ text = "", texthl = "LspDiagnosticsDefaultInformation" })
+--vim.fn.sign_define("LspDiagnosticsSignHint",{ text = "", texthl = "LspDiagnosticsDefaultHint" })
+vim.fn.sign_define(
+    "LspDiagnosticsSignError",
+    {texthl = "LspDiagnosticsSignError", text = "", numhl = "LspDiagnosticsSignError"}
+)
+vim.fn.sign_define(
+    "LspDiagnosticsSignWarning",
+    {texthl = "LspDiagnosticsSignWarning", text = "", numhl = "LspDiagnosticsSignWarning"}
+)
+vim.fn.sign_define(
+    "LspDiagnosticsSignHint",
+    {texthl = "LspDiagnosticsSignHint", text = "", numhl = "LspDiagnosticsSignHint"}
+)
+vim.fn.sign_define(
+    "LspDiagnosticsSignInformation",
+    {texthl = "LspDiagnosticsSignInformation", text = "", numhl = "LspDiagnosticsSignInformation"}
+)
+
+-- replace lsp-kind
+vim.lsp.protocol.CompletionItemKind = {
+    "   (Text) ",
+    "   (Method)",
+    "   (Function)",
+    "   (Constructor)",
+    " ﴲ  (Field)",
+    "[] (Variable)",
+    "   (Class)",
+    " ﰮ  (Interface)",
+    "   (Module)",
+    " 襁 (Property)",
+    "   (Unit)",
+    "   (Value)",
+    " 練 (Enum)",
+    "   (Keyword)",
+    " ﬌  (Snippet)",
+    "   (Color)",
+    "   (File)",
+    "   (Reference)",
+    "   (Folder)",
+    "   (EnumMember)",
+    " ﲀ  (Constant)",
+    " ﳤ  (Struct)",
+    "   (Event)",
+    "   (Operator)",
+    "   (TypeParameter)"
+}
 
 vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
