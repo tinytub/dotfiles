0f4397cee0b9e609df6e653952a751467b833e24
tinytub <zhaopeng1988@mail.com>
2021-06-16 16:40:14 +0800

    change module ?

---
+++ config/nvim/lua/folke-which-key/init.lua
@@ -0,0 +1,263 @@
+require("which-key").setup {
+    plugins = {
+        marks = true, -- shows a list of your marks on ' and `
+        registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+        -- the presets plugin, adds help for a bunch of default keybindings in Neovim
+        -- No actual key bindings are created
+        presets = {
+            operators = false, -- adds help for operators like d, y, ...
+            motions = false, -- adds help for motions
+            text_objects = false, -- help for text objects triggered after entering an operator
+            windows = true, -- default bindings on <c-w>
+            nav = true, -- misc bindings to work with windows
+            z = true, -- bindings for folds, spelling and others prefixed with z
+            g = true -- bindings for prefixed with g
+        }
+    },
+    icons = {
+        breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+        separator = "➜", -- symbol used between a key and it's label
+        group = "+" -- symbol prepended to a group
+    },
+    window = {
+        border = "single", -- none, single, double, shadow
+        position = "bottom", -- bottom, top
+        margin = {1, 0, 1, 0}, -- extra window margin [top, right, bottom, left]
+        padding = {2, 2, 2, 2} -- extra window padding [top, right, bottom, left]
+    },
+    layout = {
+        height = {min = 4, max = 25}, -- min and max height of the columns
+        width = {min = 20, max = 50}, -- min and max width of the columns
+        spacing = 3 -- spacing between columns
+    },
+    hidden = {"<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ "}, -- hide mapping boilerplate
+    show_help = true -- show help message on the command line when the popup is visible
+}
+
+local opts = {
+    mode = "n", -- NORMAL mode
+    prefix = "<leader>",
+    buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+    silent = true, -- use `silent` when creating keymaps
+    noremap = true, -- use `noremap` when creating keymaps
+    nowait = false -- use `nowait` when creating keymaps
+}
+
+local mappings = {
+    --["/"] = "Comment",
+    ["c"] = "Close Buffer",
+    ["e"] = {"<cmd>NvimTreeToggle<cr>"                                 ,"Explorer"},
+    ["F"] = {"<cmd>NvimTreeFindFile<cr>"                               ,"Find Current File"},
+    ["V"] = {"<cmd>Vista<cr>"                                          ,"Vista"},
+    [";"] = {"<cmd>Dashboard<cr>"                                        , "home screen"},
+    ["M"] = {"<cmd>MarkdownPreviewToggle<cr>"                          , "markdown preview"},
+    ["h"] = {"<cmd>let @/ = \"\"<cr>"                                    , "no highlight" },
+    ["T"] = {"<cmd>TSHighlightCapturesUnderCursor<cr>"                 , "treesitter highlight" },
+    ["v"] = {"<C-W>v"                                          , "split right"},
+
+    a = {
+      name= '+actions' ,
+      ['c'] = {"<cmd>ColorizerToggle<cr>"        , 'colorizer'},
+      ['h'] = {"<cmd>let @/ = \"\"<cr>"            , 'remove search highlight'},
+      ['i'] = {"<cmd>IndentBlanklineToggle<cr>"  , 'toggle indent lines'},
+      ['n'] = {"<cmd>set nonumber!<cr>"          , 'line-numbers'},
+      ['r'] = {"<cmd>set norelativenumber!<cr>"  , 'relative line nums'},
+    },
+    f = {
+        name = "+Finder",
+        r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+        n = { "<cmd>enew<cr>", "New File" },
+        --. = {"<cmd>Telescope filetypes<cr>"                   , "filetypes"},
+        B = {"<cmd>Telescope git_branches<cr>"                , "git branches"},
+        d = {"<cmd>Telescope lsp_document_diagnostics<cr>"    , "document_diagnostics"},
+        D = {"<cmd>Telescope lsp_workspace_diagnostics<cr>"   , "workspace_diagnostics"},
+        f = {"<cmd>Telescope find_files<cr>"                 , "files"},
+        h = {"<cmd>Telescope command_history<cr>"            , "history"},
+        i = {"<cmd>Telescope media_files<cr>"                , "media files"},
+        m = {"<cmd>Telescope marks<cr>"                      , "marks"},
+        M = {"<cmd>Telescope man_pages<cr>"                  , "man_pages"},
+        v = {"<cmd>Telescope vim_options<cr>"                , "vim_options"},
+        a = {"<cmd>Telescope live_grep<cr>"                  , "text"},
+        b = {"<cmd>Telescope file_browser<cr>"               , "file browser"},
+        u = {"<cmd>Telescope colorscheme<cr>"                , "colorschemes"},
+        g = {"<cmd>Telescope git_files<cr>"                  , "git_files"},
+        w = {"<cmd>Telescope grep_string<cr>"                , "grep_string"},
+        o = {"<cmd>Telescope oldfiles<cr>"                   , "oldfiles"},
+        c = {"<cmd>Telescope git_commits<cr>"                , "git_commits"},
+        s = {"<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols"},
+        S = {"<cmd>Telescope lsp_workspace_symbols<cr>", "Workspace Symbols"},
+        q = {"<cmd>Telescope quickfix<cr>", "Quickfix"},
+      },
+
+    b = {
+      name = '+buffer' ,
+      ['>'] = {"<cmd>BufferMoveNext<cr>"        , 'move next'},
+      ['<'] = {"<cmd>BufferMovePrevious<cr>"    , 'move prev'},
+      ['P'] = {"<cmd>BufferPick<cr>"            , 'pick buffer'},
+      ['d'] = {"<cmd>BufferClose<cr>"           , 'delete-buffer'},
+      ['n'] = {"bnext"                  , 'next-buffer'},
+      ['p'] = {"bprevious"              , 'previous-buffer'},
+      ['?'] = {"Buffers"                , 'fzf-buffer'},
+      ['b'] = {"<cmd>Telescope buffers<cr>"                , 'find bufers'},
+    },
+    d = {
+        name = "+Debug",
+        b = {"<cmd>DebugToggleBreakpoint<cr>", "Toggle Breakpoint"},
+        c = {"<cmd>DebugContinue<cr>", "Continue"},
+        i = {"<cmd>DebugStepInto<cr>", "Step Into"},
+        o = {"<cmd>DebugStepOver<cr>", "Step Over"},
+        r = {"<cmd>DebugToggleRepl<cr>", "Toggle Repl"},
+        s = {"<cmd>DebugStart<cr>", "Start"},
+    },
+    g = {
+        name = "+Git",
+        b = {"<cmd>Git blame<cr>", "Git Blame"},
+        d = {"<cmd>Git diff<cr>", "Git Diff"},
+        l = {"<cmd>Git log<cr>", "Git Log"},
+        s = {"<cmd>Git status<cr>", "Git Status"},
+    },
+    l = {
+        name = "+LSP",
+        a = {"<cmd>Lspsaga code_action<cr>", "Code Action"},
+        A = {"<cmd>Lspsaga range_code_action<cr>", "Selected Action"},
+--        d = {"<cmd>Telescope lsp_document_diagnostics<cr>", "Document Diagnostics"},
+--        D = {"<cmd>Telescope lsp_workspace_diagnostics<cr>", "Workspace Diagnostics"},
+--        f = {"<cmd>LspFormatting<cr>", "Format"},
+--       i = {"<cmd>LspInfo<cr>", "Info"},
+        l = {"<cmd>Lspsaga lsp_finder<cr>", "LSP Finder"},
+        L = {"<cmd>Lspsaga show_line_diagnostics<cr>", "Line Diagnostics"},
+        p = {"<cmd>Lspsaga preview_definition<cr>", "Preview Definition"},
+        r = {"<cmd>Lspsaga rename<cr>", "Rename"},
+--        t = {"<cmd>LspTypeDefinition<cr>", "Type Definition"},
+        x = {"<cmd>cclose<cr>", "Close Quickfix"},
+    },
+    t = {
+       name = '+Terminal' ,
+       [";"] = {"<cmd>FloatermNew --wintype=normal --height=6<cr>"       , "terminal"},
+       ["f"] = {"<cmd>FloatermNew fzf<cr>"                               , "fzf"},
+       ["g"] = {"<cmd>FloatermNew lazygit<cr>"                           , "git"},
+       ["n"] = {"<cmd>FloatermNew node<cr>"                              , "node"},
+       ["p"] = {"<cmd>FloatermNew python<cr>"                            , "python"},
+       ["t"] = {"<cmd>FloatermToggle<cr>"                                , "toggle"},
+       ["k"] = {"<cmd>FloatermKill<cr>"                                  , "kill term"},
+    },
+    p = {
+        name = 'Packer' ,
+        ["u"]  = {"<cmd>PackerUpdate<cr>"      ,  'packer update'},
+        ["i"]  = {"<cmd>PackerInstall<cr>"     ,  'packer install'},
+        ["c"]  = {"<cmd>PackerCompile<cr>"     ,  'packer compile'},
+        ["s"]  = {"<cmd>PackerSync<cr>"        ,  'packer sync'},
+    },
+    S = {name = "+Session", s = {"<cmd>SessionSave<cr>", "Save Session"}, l = {"<cmd>SessionLoad<cr>", "Load Session"}}
+}
+
+--local mappings = {
+--    --["/"] = "Comment",
+--    ["c"] = "Close Buffer",
+--    ["e"] = {"<cmd>NvimTreeToggle<cr>"                                 ,"Explorer"},
+--    ["F"] = {"<cmd>NvimTreeFindFile<cr>"                               ,"Find Current File"},
+--    ["V"] = {"<cmd>Vista<cr>"                                          ,"Vista"},
+--    [";"] = {"<cmd>Dashboard<cr>"                                        , "home screen"},
+--    ["M"] = {"<cmd>MarkdownPreviewToggle<cr>"                          , "markdown preview"},
+--    ["h"] = {"<cmd>let @/ = \"\"<cr>"                                    , "no highlight" },
+--    ["T"] = {"<cmd>TSHighlightCapturesUnderCursor<cr>"                 , "treesitter highlight" },
+--    ["v"] = {"<C-W>v"                                          , "split right"},
+--
+--    a = {
+--      name= '+actions' ,
+--      ['c'] = {"<cmd>ColorizerToggle<cr>"        , 'colorizer'},
+--      ['h'] = {"<cmd>let @/ = \"\"<cr>"            , 'remove search highlight'},
+--      ['i'] = {"<cmd>IndentBlanklineToggle<cr>"  , 'toggle indent lines'},
+--      ['n'] = {"<cmd>set nonumber!<cr>"          , 'line-numbers'},
+--      ['r'] = {"<cmd>set norelativenumber!<cr>"  , 'relative line nums'},
+--    },
+--    f = {
+--        name = "+Finder",
+--        r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+--        n = { "<cmd>enew<cr>", "New File" },
+--        --. = {"<cmd>Telescope filetypes<cr>"                   , "filetypes"},
+--        B = {"<cmd>Telescope git_branches<cr>"                , "git branches"},
+--        d = {"<cmd>Telescope lsp_document_diagnostics<cr>"    , "document_diagnostics"},
+--        D = {"<cmd>Telescope lsp_workspace_diagnostics<cr>"   , "workspace_diagnostics"},
+--        f = {"<cmd>Telescope find_files<cr>"                 , "files"},
+--        h = {"<cmd>Telescope command_history<cr>"            , "history"},
+--        i = {"<cmd>Telescope media_files<cr>"                , "media files"},
+--        m = {"<cmd>Telescope marks<cr>"                      , "marks"},
+--        M = {"<cmd>Telescope man_pages<cr>"                  , "man_pages"},
+--        v = {"<cmd>Telescope vim_options<cr>"                , "vim_options"},
+--        a = {"<cmd>Telescope live_grep<cr>"                  , "text"},
+--        b = {"<cmd>Telescope file_browser<cr>"               , "file browser"},
+--        u = {"<cmd>Telescope colorscheme<cr>"                , "colorschemes"},
+--        g = {"<cmd>Telescope git_files<cr>"                  , "git_files"},
+--        w = {"<cmd>Telescope grep_string<cr>"                , "grep_string"},
+--        o = {"<cmd>Telescope oldfiles<cr>"                   , "oldfiles"},
+--        c = {"<cmd>Telescope git_commits<cr>"                , "git_commits"},
+--        s = {"<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols"},
+--        S = {"<cmd>Telescope lsp_workspace_symbols<cr>", "Workspace Symbols"},
+--        q = {"<cmd>Telescope quickfix<cr>", "Quickfix"},
+--      },
+--
+--    b = {
+--      name = '+buffer' ,
+--      ['>'] = {"<cmd>BufferMoveNext<cr>"        , 'move next'},
+--      ['<'] = {"<cmd>BufferMovePrevious<cr>"    , 'move prev'},
+--      ['P'] = {"<cmd>BufferPick<cr>"            , 'pick buffer'},
+--      ['d'] = {"<cmd>BufferClose<cr>"           , 'delete-buffer'},
+--      ['n'] = {"bnext"                  , 'next-buffer'},
+--      ['p'] = {"bprevious"              , 'previous-buffer'},
+--      ['?'] = {"Buffers"                , 'fzf-buffer'},
+--      ['b'] = {"<cmd>Telescope buffers<cr>"                , 'find bufers'},
+--    },
+--    d = {
+--        name = "+Debug",
+--        b = {"<cmd>DebugToggleBreakpoint<cr>", "Toggle Breakpoint"},
+--        c = {"<cmd>DebugContinue<cr>", "Continue"},
+--        i = {"<cmd>DebugStepInto<cr>", "Step Into"},
+--        o = {"<cmd>DebugStepOver<cr>", "Step Over"},
+--        r = {"<cmd>DebugToggleRepl<cr>", "Toggle Repl"},
+--        s = {"<cmd>DebugStart<cr>", "Start"},
+--    },
+--    g = {
+--        name = "+Git",
+--        b = {"<cmd>Git blame<cr>", "Git Blame"},
+--        d = {"<cmd>Git diff<cr>", "Git Diff"},
+--        l = {"<cmd>Git log<cr>", "Git Log"},
+--        s = {"<cmd>Git status<cr>", "Git Status"},
+--    },
+--    l = {
+--        name = "+LSP",
+--        a = {"<cmd>Lspsaga code_action<cr>", "Code Action"},
+--        A = {"<cmd>Lspsaga range_code_action<cr>", "Selected Action"},
+----        d = {"<cmd>Telescope lsp_document_diagnostics<cr>", "Document Diagnostics"},
+----        D = {"<cmd>Telescope lsp_workspace_diagnostics<cr>", "Workspace Diagnostics"},
+----        f = {"<cmd>LspFormatting<cr>", "Format"},
+----       i = {"<cmd>LspInfo<cr>", "Info"},
+--        l = {"<cmd>Lspsaga lsp_finder<cr>", "LSP Finder"},
+--        L = {"<cmd>Lspsaga show_line_diagnostics<cr>", "Line Diagnostics"},
+--        p = {"<cmd>Lspsaga preview_definition<cr>", "Preview Definition"},
+--        r = {"<cmd>Lspsaga rename<cr>", "Rename"},
+----        t = {"<cmd>LspTypeDefinition<cr>", "Type Definition"},
+--        x = {"<cmd>cclose<cr>", "Close Quickfix"},
+--    },
+--    t = {
+--       name = '+Terminal' ,
+--       [";"] = {"<cmd>FloatermNew --wintype=normal --height=6<cr>"       , "terminal"},
+--       ["f"] = {"<cmd>FloatermNew fzf<cr>"                               , "fzf"},
+--       ["g"] = {"<cmd>FloatermNew lazygit<cr>"                           , "git"},
+--       ["n"] = {"<cmd>FloatermNew node<cr>"                              , "node"},
+--       ["p"] = {"<cmd>FloatermNew python<cr>"                            , "python"},
+--       ["t"] = {"<cmd>FloatermToggle<cr>"                                , "toggle"},
+--       ["k"] = {"<cmd>FloatermKill<cr>"                                  , "kill term"},
+--    },
+--    p = {
+--        name = 'Packer' ,
+--        ["u"]  = {"<cmd>PackerUpdate<cr>"      ,  'packer update'},
+--        ["i"]  = {"<cmd>PackerInstall<cr>"     ,  'packer install'},
+--        ["c"]  = {"<cmd>PackerCompile<cr>"     ,  'packer compile'},
+--        ["s"]  = {"<cmd>PackerSync<cr>"        ,  'packer sync'},
+--    },
+--    S = {name = "+Session", s = {"<cmd>SessionSave<cr>", "Save Session"}, l = {"<cmd>SessionLoad<cr>", "Load Session"}}
+--  }
+
+local wk = require("which-key")
+wk.register(mappings, opts)
---
+++ config/nvim/lua/galaxyline-nvim/init.lua
@@ -0,0 +1,509 @@
+local gl = require("galaxyline")
+local gls = gl.section
+local condition = require('galaxyline.condition')
+local fn = vim.fn
+
+gl.short_line_list = {"LuaTree", "vista", "dbui",'NvimTree', 'vista', 'dbui', 'packer'}
+
+local colors = {
+    bg = "#1e222a",
+    line_bg = "#1e222a",
+    fg = "#D8DEE9",
+    fg_green = "#65a380",
+    yellow = "#A3BE8C",
+    cyan = "#22262C",
+    darkblue = "#61afef",
+    green = "#BBE67E",
+    orange = "#FF8800",
+    purple = "#252930",
+    magenta = "#c678dd",
+    blue = "#22262C",
+    red = "#DF8890",
+    lightbg = "#282c34",
+    nord = "#81A1C1",
+    greenYel = "#EBCB8B"
+}
+
+gls.left[1] = {
+    leftRounded = {
+        provider = function()
+            return ""
+        end,
+        highlight = {colors.nord, colors.bg}
+    }
+}
+
+gls.left[2] = {
+    ViMode = {
+        provider = function()
+            return "   "
+        end,
+        highlight = {colors.bg, colors.nord},
+        separator = " ",
+        separator_highlight = {colors.lightbg, colors.lightbg}
+    }
+}
+
+gls.left[3] = {
+    FileIcon = {
+        provider = "FileIcon",
+        condition = condition.buffer_not_empty,
+        highlight = {require("galaxyline.provider_fileinfo").get_file_icon_color, colors.lightbg}
+    }
+}
+
+gls.left[4] = {
+    FileName = {
+        --provider = {"FileName", "FileSize"},
+        provider = function()
+            return fn.expand("%:F")
+        end,
+        condition = condition.buffer_not_empty,
+        highlight = {colors.fg, colors.lightbg}
+    }
+}
+
+gls.left[5] = {
+    teech = {
+        provider = function()
+            return ""
+        end,
+        separator = " ",
+        highlight = {colors.lightbg, colors.line_bg}
+    }
+}
+
+local checkwidth = function()
+    local squeeze_width = vim.fn.winwidth(0) / 2
+    if squeeze_width > 40 then
+        return true
+    end
+    return false
+end
+
+gls.left[6] = {
+    DiffAdd = {
+        provider = "DiffAdd",
+        condition = checkwidth,
+        icon = "   ",
+        highlight = {colors.greenYel, colors.line_bg}
+    }
+}
+
+gls.left[7] = {
+    DiffModified = {
+        provider = "DiffModified",
+        condition = checkwidth,
+        icon = " ",
+        highlight = {colors.orange, colors.line_bg}
+    }
+}
+
+gls.left[8] = {
+    DiffRemove = {
+        provider = "DiffRemove",
+        condition = checkwidth,
+        icon = " ",
+        highlight = {colors.red, colors.line_bg}
+    }
+}
+
+gls.left[9] = {
+    LeftEnd = {
+        provider = function()
+            return " "
+        end,
+        separator = " ",
+        separator_highlight = {colors.line_bg, colors.line_bg},
+        highlight = {colors.line_bg, colors.line_bg}
+    }
+}
+
+gls.left[10] = {
+    DiagnosticError = {
+        provider = "DiagnosticError",
+        icon = "  ",
+        highlight = {colors.red, colors.bg}
+    }
+}
+
+gls.left[11] = {
+    Space = {
+        provider = function()
+            return " "
+        end,
+        highlight = {colors.line_bg, colors.line_bg}
+    }
+}
+
+gls.left[12] = {
+    DiagnosticWarn = {
+        provider = "DiagnosticWarn",
+        icon = "  ",
+        highlight = {colors.blue, colors.bg}
+    }
+}
+
+--gls.mid[1] = {
+--    ShowLspClient = {
+--      provider = 'GetLspClient',
+--      condition = function ()
+--        local tbl = {['dashboard'] = true,['']=true}
+--        if tbl[vim.bo.filetype] then
+--          return false
+--        end
+--        return true
+--      end,
+--      icon = ' LSP:',
+--      highlight = {colors.yellow, colors.bg}
+--    }
+--}
+
+gls.right[1] = {
+  FileEncode = {
+    provider = 'FileEncode',
+    condition = condition.hide_in_width,
+    separator = ' ',
+    separator_highlight = {'NONE',colors.bg},
+    --highlight = {colors.green,colors.bg,'bold'}
+    highlight = {colors.fg, colors.bg,'bold'}
+  }
+}
+
+gls.right[2] = {
+  FileFormat = {
+    provider = 'FileFormat',
+    condition = condition.hide_in_width,
+    separator = ' ',
+    separator_highlight = {'NONE',colors.bg},
+    --highlight = {colors.green,colors.bg,'bold'}
+    highlight = {colors.fg, colors.bg,'bold'}
+  }
+}
+
+gls.right[4] = {
+    GitIcon = {
+        provider = function()
+            return "   "
+        end,
+        condition = require("galaxyline.provider_vcs").check_git_workspace,
+
+        separator = ' ',
+        separator_highlight = {'NONE',colors.bg},
+        highlight = {colors.green, colors.line_bg}
+    }
+}
+
+gls.right[5] = {
+    GitBranch = {
+        provider = "GitBranch",
+        condition = require("galaxyline.provider_vcs").check_git_workspace,
+        highlight = {colors.green, colors.line_bg}
+    }
+}
+
+gls.right[6] = {
+    TreesitterIcon = {
+        provider = function()
+            if next(vim.treesitter.highlighter.active) ~= nil then return ' ' end
+            return ''
+        end,
+        separator = ' ',
+        separator_highlight = {'NONE', colors.bg},
+        highlight = {colors.green, colors.bg}
+    }
+}
+
+gls.right[7] = {
+    right_LeftRounded = {
+        provider = function()
+            return ""
+        end,
+        separator = " ",
+        separator_highlight = {colors.bg, colors.bg},
+        highlight = {colors.red, colors.bg}
+    }
+}
+
+gls.right[8] = {
+    SiMode = {
+        provider = function()
+            local alias = {
+                n = "NORMAL",
+                i = "INSERT",
+                c = "COMMAND",
+                V = "VISUAL",
+                [""] = "VISUAL",
+                v = "VISUAL",
+                R = "REPLACE"
+            }
+            return alias[vim.fn.mode()]
+        end,
+        highlight = {colors.bg, colors.red}
+    }
+}
+
+gls.right[9] = {
+    PerCent = {
+        provider = "LinePercent",
+        separator = " ",
+        separator_highlight = {colors.red, colors.red},
+        highlight = {colors.bg, colors.fg}
+    }
+}
+
+gls.right[10] = {
+    rightRounded = {
+        provider = function()
+            return ""
+        end,
+        highlight = {colors.fg, colors.bg}
+    }
+}
+
+gls.short_line_left[1] = {
+  BufferType = {
+    provider = 'FileTypeName',
+    separator = ' ',
+    separator_highlight = {'NONE',colors.bg},
+    highlight = {colors.blue,colors.bg,'bold'}
+  }
+}
+
+gls.short_line_left[2] = {
+  SFileName = {
+    --provider =  'SFileName',
+   provider = function()
+        local fileinfo = require("galaxyline.provider_fileinfo")
+        local fname = fileinfo.get_current_file_name()
+        for _, v in ipairs(gl.short_line_list) do
+          if v == vim.bo.filetype then
+            return ""
+          end
+        end
+        return fname
+      end,
+    condition = condition.buffer_not_empty,
+    highlight = {colors.fg,colors.bg,'bold'}
+  }
+}
+
+gls.short_line_right[1] = {
+  BufferIcon = {
+    provider= 'BufferIcon',
+    highlight = {colors.fg,colors.bg}
+  }
+}
+
+--local gl = require('galaxyline')
+--local colors = require('galaxyline.theme').default
+--local condition = require('galaxyline.condition')
+--local gls = gl.section
+--
+--gl.short_line_list = {'NvimTree','vista','dbui','packer'}
+--
+--gls.left[1] = {
+--  RainbowRed = {
+--    provider = function() return '▊ ' end,
+--    highlight = {colors.blue,colors.bg}
+--  },
+--}
+--gls.left[2] = {
+--  ViMode = {
+--    provider = function()
+--      -- auto change color according the vim mode
+--      local mode_color = {n = colors.red, i = colors.green,v=colors.blue,
+--                          [''] = colors.blue,V=colors.blue,
+--                          c = colors.magenta,no = colors.red,s = colors.orange,
+--                          S=colors.orange,[''] = colors.orange,
+--                          ic = colors.yellow,R = colors.violet,Rv = colors.violet,
+--                          cv = colors.red,ce=colors.red, r = colors.cyan,
+--                          rm = colors.cyan, ['r?'] = colors.cyan,
+--                          ['!']  = colors.red,t = colors.red}
+--      vim.api.nvim_command('hi GalaxyViMode guifg='..mode_color[vim.fn.mode()])
+--      return '  '
+--    end,
+--    highlight = {colors.red,colors.bg,'bold'},
+--  },
+--}
+--gls.left[3] = {
+--  FileSize = {
+--    provider = 'FileSize',
+--    condition = condition.buffer_not_empty,
+--    highlight = {colors.fg,colors.bg}
+--  }
+--}
+--gls.left[4] ={
+--  FileIcon = {
+--    provider = 'FileIcon',
+--    condition = condition.buffer_not_empty,
+--    highlight = {require('galaxyline.provider_fileinfo').get_file_icon_color,colors.bg},
+--  },
+--}
+--
+--gls.left[5] = {
+--  FileName = {
+--    provider = 'FileName',
+--    condition = condition.buffer_not_empty,
+--    highlight = {colors.fg,colors.bg,'bold'}
+--  }
+--}
+--
+--gls.left[6] = {
+--  LineInfo = {
+--    provider = 'LineColumn',
+--    separator = ' ',
+--    separator_highlight = {'NONE',colors.bg},
+--    highlight = {colors.fg,colors.bg},
+--  },
+--}
+--
+--gls.left[7] = {
+--  PerCent = {
+--    provider = 'LinePercent',
+--    separator = ' ',
+--    separator_highlight = {'NONE',colors.bg},
+--    highlight = {colors.fg,colors.bg,'bold'},
+--  }
+--}
+--
+--gls.left[8] = {
+--  DiagnosticError = {
+--    provider = 'DiagnosticError',
+--    icon = '  ',
+--    highlight = {colors.red,colors.bg}
+--  }
+--}
+--gls.left[9] = {
+--  DiagnosticWarn = {
+--    provider = 'DiagnosticWarn',
+--    icon = '  ',
+--    highlight = {colors.yellow,colors.bg},
+--  }
+--}
+--
+--gls.left[10] = {
+--  DiagnosticHint = {
+--    provider = 'DiagnosticHint',
+--    icon = '  ',
+--    highlight = {colors.cyan,colors.bg},
+--  }
+--}
+--
+--gls.left[11] = {
+--  DiagnosticInfo = {
+--    provider = 'DiagnosticInfo',
+--    icon = '  ',
+--    highlight = {colors.blue,colors.bg},
+--  }
+--}
+--
+--gls.mid[1] = {
+--  ShowLspClient = {
+--    provider = 'GetLspClient',
+--    condition = function ()
+--      local tbl = {['dashboard'] = true,['']=true}
+--      if tbl[vim.bo.filetype] then
+--        return false
+--      end
+--      return true
+--    end,
+--    icon = ' LSP:',
+--    highlight = {colors.yellow,colors.bg,'bold'}
+--  }
+--}
+--
+--gls.right[1] = {
+--  FileEncode = {
+--    provider = 'FileEncode',
+--    condition = condition.hide_in_width,
+--    separator = ' ',
+--    separator_highlight = {'NONE',colors.bg},
+--    highlight = {colors.green,colors.bg,'bold'}
+--  }
+--}
+--
+--gls.right[2] = {
+--  FileFormat = {
+--    provider = 'FileFormat',
+--    condition = condition.hide_in_width,
+--    separator = ' ',
+--    separator_highlight = {'NONE',colors.bg},
+--    highlight = {colors.green,colors.bg,'bold'}
+--  }
+--}
+--
+--gls.right[3] = {
+--  GitIcon = {
+--    provider = function() return '  ' end,
+--    condition = condition.check_git_workspace,
+--    separator = ' ',
+--    separator_highlight = {'NONE',colors.bg},
+--    highlight = {colors.violet,colors.bg,'bold'},
+--  }
+--}
+--
+--gls.right[4] = {
+--  GitBranch = {
+--    provider = 'GitBranch',
+--    condition = condition.check_git_workspace,
+--    highlight = {colors.violet,colors.bg,'bold'},
+--  }
+--}
+--
+--gls.right[5] = {
+--  DiffAdd = {
+--    provider = 'DiffAdd',
+--    condition = condition.hide_in_width,
+--    icon = '  ',
+--    highlight = {colors.green,colors.bg},
+--  }
+--}
+--gls.right[6] = {
+--  DiffModified = {
+--    provider = 'DiffModified',
+--    condition = condition.hide_in_width,
+--    icon = ' 柳',
+--    highlight = {colors.orange,colors.bg},
+--  }
+--}
+--gls.right[7] = {
+--  DiffRemove = {
+--    provider = 'DiffRemove',
+--    condition = condition.hide_in_width,
+--    icon = '  ',
+--    highlight = {colors.red,colors.bg},
+--  }
+--}
+--
+--gls.right[8] = {
+--  RainbowBlue = {
+--    provider = function() return ' ▊' end,
+--    highlight = {colors.blue,colors.bg}
+--  },
+--}
+--
+--gls.short_line_left[1] = {
+--  BufferType = {
+--    provider = 'FileTypeName',
+--    separator = ' ',
+--    separator_highlight = {'NONE',colors.bg},
+--    highlight = {colors.blue,colors.bg,'bold'}
+--  }
+--}
+--
+--gls.short_line_left[2] = {
+--  SFileName = {
+--    provider =  'SFileName',
+--    condition = condition.buffer_not_empty,
+--    highlight = {colors.fg,colors.bg,'bold'}
+--  }
+--}
+--
+--gls.short_line_right[1] = {
+--  BufferIcon = {
+--    provider= 'BufferIcon',
+--    highlight = {colors.fg,colors.bg}
+--  }
+--}
+ 
