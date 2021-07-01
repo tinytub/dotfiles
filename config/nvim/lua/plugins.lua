8644940ad316dc727754595432ad9eb0abf715a8
tinytub <zhaopeng1988@mail.com>
2021-07-01 11:26:16 +0800

    update

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -49,8 +49,8 @@ return require("packer").startup(function(use)
     use {"nvim-lua/plenary.nvim"}
     use {
         "nvim-telescope/telescope.nvim", 
-        config = [[require('lv-telescope')]],    
-        cmd = "Telescope"
+        config = [[require('n-telescope')]],    
+        --cmd = "Telescope"
     }
 
     -- Autocomplete
