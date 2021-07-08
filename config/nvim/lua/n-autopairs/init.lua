921e8573a8e024b7cf15eb133e40fd3344f36251
tinytub <zhaopeng1988@mail.com>
2021-07-08 11:56:01 +0800

    update

--- config/nvim/lua/n-autopairs/init.lua
+++ config/nvim/lua/n-autopairs/init.lua
@@ -5,25 +5,6 @@ local status_ok, autopairs = pcall(require, "nvim-autopairs")
 if not status_ok then
   return
 end
-local npairs = require "nvim-autopairs"
-local Rule = require "nvim-autopairs.rule"
-
--- skip it, if you use another global object
-_G.MUtils = {}
-
-vim.g.completion_confirm_key = ""
-MUtils.completion_confirm = function()
-  if vim.fn.pumvisible() ~= 0 then
-    if vim.fn.complete_info()["selected"] ~= -1 then
-      return vim.fn["compe#confirm"](npairs.esc "<cr>")
-    else
-      return npairs.esc "<cr>"
-    end
-  else
-    return npairs.autopairs_cr()
-  end
-end
-
 if package.loaded["compe"] then
   require("nvim-autopairs.completion.compe").setup {
     map_cr = true, --  map <CR> on insert mode
@@ -31,7 +12,9 @@ if package.loaded["compe"] then
   }
 end
 
-npairs.setup {
+--print(vim.inspect(cond))
+
+autopairs.setup {
   check_ts = true,
   ts_config = {
     lua = { "string" }, -- it will not add pair on that treesitter node
@@ -41,12 +24,14 @@ npairs.setup {
   disable_filetype = { "TelescopePrompt" , "vim" },
 }
 
-require("nvim-treesitter.configs").setup { autopairs = { enable = true } }
+--require("nvim-treesitter.configs").setup { autopairs = { enable = true } }
+
 
 local ts_conds = require "nvim-autopairs.ts-conds"
-
--- press % => %% is only inside comment or string
-npairs.add_rules {
+local Rule = require "nvim-autopairs.rule"
+autopairs.add_rules {
   Rule("%", "%", "lua"):with_pair(ts_conds.is_ts_node { "string", "comment" }),
   Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node { "function" }),
 }
+
+
