1fa880254d4a9f9ee4391a291b8aca4bd6d603d6
tinytub <zhaopeng1988@gmail.com>
2021-11-22 14:26:09 +0800

    update

--- config/nvim/lua/plugins/null-ls.lua
+++ config/nvim/lua/plugins/null-ls.lua
@@ -16,6 +16,23 @@ local sources = {
       command = "eslint_d",
    },
 
+   -- Golang
+   b.formatting.golines.with {
+       filetypes = { "go" },
+       command = "golines",
+       args = {}
+   },
+   b.formatting.goimports.with {
+    filetypes = { "go" },
+    command = "goimports",
+    args = {}
+   },
+   b.formatting.gofmt.with {
+    filetypes = { "go" },
+    command = "gofmt",
+    args = {}
+   },
+
    -- Lua
    b.formatting.stylua,
    b.diagnostics.luacheck.with { extra_args = { "--global vim" } },
