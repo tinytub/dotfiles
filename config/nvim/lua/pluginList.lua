b7da0416157eccbcb51747863f35efd001a51f04
tinytub <zhaopeng1988@gmail.com>
2021-11-26 16:07:23 +0800

    update

--- config/nvim/lua/pluginList.lua
+++ config/nvim/lua/pluginList.lua
@@ -391,19 +391,29 @@ return packer.startup(
 --
 
 -- use toggleterm ?
-    use {"voldikss/vim-floaterm",
-        cmd = {'FloatermNew','FloatermPrev','FloatermNext','FloatermFirst','FloatermLast','FloatermUpdate','FloatermToggle','FloatermShow','FloatermHide','FloatermKill','FloatermSend'},
-        config = function()
-            vim.g.floaterm_gitcommit='floaterm'
-            vim.g.floaterm_autoinsert=1
-            vim.g.floaterm_width=0.8
-            vim.g.floaterm_height=0.8
-            vim.g.floaterm_wintitle=0
-            vim.g.floaterm_autoclose=1
-        end,
-        event = "BufRead",
-    }
+    --use {"voldikss/vim-floaterm",
+    --    cmd = {'FloatermNew','FloatermPrev','FloatermNext','FloatermFirst','FloatermLast','FloatermUpdate','FloatermToggle','FloatermShow','FloatermHide','FloatermKill','FloatermSend'},
+    --    config = function()
+    --        vim.g.floaterm_gitcommit='floaterm'
+    --        vim.g.floaterm_autoinsert=1
+    --        vim.g.floaterm_width=0.8
+    --        vim.g.floaterm_height=0.8
+    --        vim.g.floaterm_wintitle=0
+    --        vim.g.floaterm_autoclose=1
+    --    end,
+    --    event = "BufRead",
+    --}
 
+  -- Terminal
+    use {
+      "akinsho/toggleterm.nvim",
+      --commit = commit.toggleterm,
+      event = "BufWinEnter",
+      config = function()
+        require('plugins.toggleterm').config()
+      end
+      --disable = not lvim.builtin.terminal.active,
+    }
     -- lsp root with this nvim-tree will follow you
     --use {
     --    "ahmedkhalf/lsp-rooter.nvim",
