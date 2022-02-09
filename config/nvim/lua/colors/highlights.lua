d34cfdd6bdb002f90092dfa60e306ed59ce4830a
tinytub <zhaopeng1988@gmail.com>
2022-02-09 17:44:37 +0800

    update

--- config/nvim/lua/colors/highlights.lua
+++ config/nvim/lua/colors/highlights.lua
@@ -145,9 +145,25 @@ if transparency then
 end
 
 -- Telescope
---fg_bg("TelescopeBorder", darker_black, darker_black)
-fg_bg("TelescopeBorder", purple, darker_black)
-fg_bg("TelescopePromptBorder", purple, black2)
+----fg_bg("TelescopeBorder", darker_black, darker_black)
+--fg_bg("TelescopeBorder", purple, darker_black)
+--fg_bg("TelescopePromptBorder", purple, black2)
+--
+--fg_bg("TelescopePromptNormal", white, black2)
+--fg_bg("TelescopePromptPrefix", red, black2)
+--
+--bg("TelescopeNormal", darker_black)
+--
+--fg_bg("TelescopePreviewTitle", black, green)
+--fg_bg("TelescopePromptTitle", black, red)
+--fg_bg("TelescopeResultsTitle", black, blue)
+--
+--bg("TelescopeSelection", black2)
+
+
+-- Telescope
+fg_bg("TelescopeBorder", darker_black, darker_black)
+fg_bg("TelescopePromptBorder", black2, black2)
 
 fg_bg("TelescopePromptNormal", white, black2)
 fg_bg("TelescopePromptPrefix", red, black2)
@@ -156,7 +172,7 @@ bg("TelescopeNormal", darker_black)
 
 fg_bg("TelescopePreviewTitle", black, green)
 fg_bg("TelescopePromptTitle", black, red)
-fg_bg("TelescopeResultsTitle", black, blue)
+fg_bg("TelescopeResultsTitle", darker_black, darker_black)
 
 bg("TelescopeSelection", black2)
 
--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -16,7 +16,6 @@ telescope.setup {
         selection_caret = " ",
         entry_prefix = "  ",
         initial_mode = "insert",
-        -- initial_mode = "insert",
         selection_strategy = "reset",
         sorting_strategy = "descending",
         layout_strategy = "horizontal",
@@ -39,11 +38,24 @@ telescope.setup {
         grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
         qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
 
+        --layout_config = {
+        --    width = 0.75,
+        --    preview_cutoff = 120,
+        --    horizontal = {mirror = false},
+        --    vertical = {mirror = false}
+        --},
         layout_config = {
-            width = 0.75,
-            preview_cutoff = 120,
-            horizontal = {mirror = false},
-            vertical = {mirror = false}
+           horizontal = {
+              prompt_position = "top",
+              preview_width = 0.55,
+              results_width = 0.8,
+           },
+           vertical = {
+              mirror = false,
+           },
+           width = 0.87,
+           height = 0.80,
+           preview_cutoff = 120,
         },
 
         ---- Developer configurations: Not meant for general override
