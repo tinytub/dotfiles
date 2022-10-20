763e9233d9033f16ff4bc402136f0dff9822a5c8
tinytub <zhaopeng1988@gmail.com>
2022-10-20 17:06:11 +0800

    update: add neotest

--- config/nvim/lua/plugins/nvim-notify.lua
+++ config/nvim/lua/plugins/nvim-notify.lua
@@ -6,6 +6,7 @@ M.config = function()
     return
   end
   notify.setup({
+    background_colour = "#000000",
     -- Animation style (see below for details)
     stages = "fade_in_slide_out",
 
@@ -20,10 +21,6 @@ M.config = function()
 
     -- Default timeout for notifications
     timeout = 5000,
-
-    -- For stages that change opacity this is treated as the highlight behind the window
-    -- Set this to either a highlight group or an RGB hex value e.g. "#000000"
-    background_colour = "Normal",
 
     -- Minimum width for notification windows
     minimum_width = 10,
--- config/nvim/lua/plugins/pluginList.lua
+++ config/nvim/lua/plugins/pluginList.lua
@@ -642,12 +642,12 @@ return packer.startup(function()
       vim.g["test#go#gotest#options"] = "-v --count=1"
       vim.g["test#echo_command"] = 1
     end,
-    disable = false,
+    disable = true,
   })
 
   use {
     'nvim-neotest/neotest',
-    disable = true,
+    disable = false,
     requires = {
       'nvim-lua/plenary.nvim',
       --'nvim-treesitter/nvim-treesitter',
@@ -657,44 +657,7 @@ return packer.startup(function()
       'akinsho/neotest-go',
     },
     config = function()
-      require('neotest').setup({
-        icons = {
-          running = ''
-        },
-        adapters = {
-          require("neotest-plenary"),
-          require('neotest-go'),
-        }
-      })
-    end,
-    setup = function()
-      vim.api.nvim_create_user_command('Neotest', function()
-        require('neotest').run.run()
-      end, { desc = 'Neotest: run nearest test' })
-      vim.api.nvim_create_user_command('NeotestFile', function()
-        require('neotest').run.run(vim.fn.expand '%')
-      end, { desc = 'Neotest: run the current file' })
-      vim.api.nvim_create_user_command('NeotestStop', function()
-        require('neotest').run.stop()
-      end, { desc = 'Neotest: stop the nearest test' })
-
-      vim.keymap.set('n', '<leader>tf', function()
-        require('neotest').run.run(vim.fn.expand '%')
-      end, { desc = 'Neotest: run all tests in file' })
-
-      vim.keymap.set('n', '<leader>tn', function()
-        require('neotest').run.run()
-      end, { desc = 'Neotest: run nearest test' })
-
-      vim.keymap.set('n', '<leader>tl', function()
-        require('neotest').run.run_last()
-      end, { desc = 'Neotest: run last test' })
-      vim.keymap.set('n', '<leader>ts', function()
-        require('neotest').summary.toggle()
-      end, { desc = 'Neotest: toggle summary' })
-      vim.keymap.set('n', '<leader>to', function()
-        require('neotest').output.open { enter = true }
-      end, { desc = 'Neotest: show test output' })
+      require("plugins.neotest")
     end,
   }
 
