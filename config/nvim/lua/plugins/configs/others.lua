c6782dcc98b2d8123ff179c29940eecf012bf1fd
tinytub <zhaopeng1988@gmail.com>
2023-04-18 10:52:16 +0800

    update

--- config/nvim/lua/plugins/configs/others.lua
+++ config/nvim/lua/plugins/configs/others.lua
@@ -33,7 +33,7 @@ M.blankline = function()
     show_trailing_blankline_indent = false,
     show_first_indent_level = false,
     show_current_context = false,
-    show_current_context_start = true,
+    --show_current_context_start = true,
   })
 end
 
--- config/nvim/lua/plugins/lazy_plugins.lua
+++ config/nvim/lua/plugins/lazy_plugins.lua
@@ -674,6 +674,9 @@ local plugins = {
         },
       },
     },
+    deactivate = function()
+      vim.cmd([[Neotree close]])
+    end,
     init = function()
       vim.g.neo_tree_remove_legacy_commands = 1
       if vim.fn.argc() == 1 then
@@ -690,7 +693,6 @@ local plugins = {
 
   {
     "lewis6991/gitsigns.nvim",
-    ft = "gitcommit",
     event = { "BufReadPre", "BufNewFile" },
     enabled = true,
     --cond = function()
@@ -891,6 +893,37 @@ local plugins = {
   },
 
   -- F 键查询增强
+  {
+    "ggandor/flit.nvim",
+    keys = function()
+      ---@type LazyKeys[]
+      local ret = {}
+      for _, key in ipairs({ "f", "F", "t", "T" }) do
+        ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
+      end
+      return ret
+    end,
+    opts = { labeled_modes = "nx" },
+  },
+  {
+    -- flit dependence
+    "ggandor/leap.nvim",
+    keys = {
+      { "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+      { "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+      { "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+    },
+    config = function(_, opts)
+      local leap = require("leap")
+      for k, v in pairs(opts) do
+        leap.opts[k] = v
+      end
+      leap.add_default_mappings(true)
+      vim.keymap.del({ "x", "o" }, "x")
+      vim.keymap.del({ "x", "o" }, "X")
+    end,
+  },
+
   {
     enabled = false,
     "unblevable/quick-scope",
