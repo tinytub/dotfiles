485054bc65d8e547ce670d84ad626264ffa225a9
tinytub <zhaopeng1988@gmail.com>
2023-06-27 10:59:32 +0800

    update

--- config/nvim/lua/plugins/configs/nvim-cmp.lua
+++ config/nvim/lua/plugins/configs/nvim-cmp.lua
@@ -234,24 +234,3 @@ return {
     end
   },
 }
-
-
----- nvim-cmp setup
-----cmp.setup()
----- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
---cmp.setup.cmdline('/', {
---  mapping = cmp.mapping.preset.cmdline(),
---  sources = {
---    { name = 'nvim_lsp' },
---    { name = 'buffer' }
---  }
---})
---
----- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
---cmp.setup.cmdline(':', {
---  mapping = cmp.mapping.preset.cmdline(),
---  sources = cmp.config.sources({
---    { name = 'path' },
---    { name = 'cmdline' }
---  })
---})
--- config/nvim/lua/plugins/lazy_plugins.lua
+++ config/nvim/lua/plugins/lazy_plugins.lua
@@ -554,29 +554,103 @@ local plugins = {
       {
         "nvim-treesitter/nvim-treesitter-textobjects",
         init = function()
-          -- PERF: no need to load the plugin, if we only need its queries for mini.ai
-          local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
-          local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-          local enabled = false
-          if opts.textobjects then
-            for _, mod in ipairs { "move", "select", "swap", "lsp_interop" } do
-              if opts.textobjects[mod] and opts.textobjects[mod].enable then
-                enabled = true
-                break
-              end
-            end
-          end
-          if not enabled then require("lazy.core.loader").disable_rtp_plugin "nvim-treesitter-textobjects" end
+          -- disable rtp plugin, as we only need its queries for mini.ai
+          -- In case other textobject modules are enabled, we will load them
+          -- once nvim-treesitter is loaded
+          require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+          load_textobjects = true
         end,
       },
     },
-    --    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSEnable", "TSDisable", "TSModuleInfo" },
-    config = function() require "plugins.configs.treesitter" end,
-    --run = ":TSUpdate",
-    run = function() require("nvim-treesitter.install").update { with_sync = true } () end,
-    --config = function ()
-    --   require "plugins.configs.treesitter"
-    --end
+    keys = {
+      { "<c-space>", desc = "Increment selection" },
+      { "<bs>",      desc = "Decrement selection", mode = "x" },
+    },
+    ---@type TSConfig
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+      ensure_installed = {
+        "bash",
+        "c",
+        "css",
+        "cpp",
+        "html",
+        "javascript",
+        "typescript",
+        "json",
+        "jsonc",
+        "jsdoc",
+        "java",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+        "toml",
+        "rust",
+        "vue",
+        "bash",
+        "go",
+        "gomod",
+        "gowork",
+        "gosum",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    },
+    ---@param opts TSConfig
+    config = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        ---@type table<string, boolean>
+        local added = {}
+        opts.ensure_installed = vim.tbl_filter(function(lang)
+          if added[lang] then
+            return false
+          end
+          added[lang] = true
+          return true
+        end, opts.ensure_installed)
+      end
+      require("nvim-treesitter.configs").setup(opts)
+
+      if load_textobjects then
+        -- PERF: no need to load the plugin, if we only need its queries for mini.ai
+        if opts.textobjects then
+          for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
+            if opts.textobjects[mod] and opts.textobjects[mod].enable then
+              local Loader = require("lazy.core.loader")
+              Loader.disabled_rtp_plugins["nvim-treesitter-textobjects"] = nil
+              local plugin = require("lazy.core.config").plugins["nvim-treesitter-textobjects"]
+              require("lazy.core.loader").source_runtime(plugin.dir, "plugin")
+              break
+            end
+          end
+        end
+      end
+    end,
+    ----    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSEnable", "TSDisable", "TSModuleInfo" },
+    --config = function() require "plugins.configs.treesitter" end,
+    ----run = ":TSUpdate",
+    --run = function() require("nvim-treesitter.install").update { with_sync = true } () end,
+    ----config = function ()
+    ----   require "plugins.configs.treesitter"
+    ----end
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
