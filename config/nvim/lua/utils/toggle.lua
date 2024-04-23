c9543f584a633bea5a0e0fa05ebe808c5520ee55
zhaopeng113 <zhaopeng18@jd.com>
2024-04-23 14:37:40 +0800

    update: with lazyvim

--- config/nvim/lua/utils/toggle.lua
+++ config/nvim/lua/utils/toggle.lua
@@ -1,4 +1,3 @@
-
 ---@class utils.toggle
 local M = {}
 
@@ -68,7 +67,7 @@ function M.inlay_hints(buf, value)
     if value == nil then
       value = not ih.is_enabled(buf)
     end
-    ih.enable(buf, value)
+    ih.enable(value, { bufnr = buf })
   end
 end
 
