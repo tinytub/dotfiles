47dc63a1c81057c72429ab42bc6958e50ac4dd60
zhaopeng113 <zhaopeng18@jd.com>
2024-07-03 10:23:35 +0800

    update:

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -6,3 +6,13 @@ local opt = vim.opt
 
 opt.relativenumber = false
 opt.wrap = true -- Enable line wrap
+
+opt.spell = false
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.softtabstop = 4
+--vim.bo.expandtab   = true
+
+--opt.title = true
+--opt.titlelen = 0 -- do not shorten title
+--opt.titlestring = 'nvim %{expand("%:p")}'
