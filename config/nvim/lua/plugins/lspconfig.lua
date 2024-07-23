70940c7ad236f75b36f406c0d01594aa409bd23f
zhaopeng113 <zhaopeng18@jd.com>
2024-07-23 11:12:14 +0800

    update: lspconfig

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -15,6 +15,60 @@ return {
       codelens = {
         enabled = false,
       },
+      servers = {
+        --  gopls = require "lsp.servers.gopls",
+        gopls = {
+          --cmd = {
+          --  "gopls", -- share the gopls instance if there is one already
+          --  "-remote.debug=:0",
+          --},
+          keys = {
+            -- Workaround for the lack of a DAP strategy in neotest-go: https://github.com/nvim-neotest/neotest-go/issues/12
+            { "<leader>td", "<cmd>lua require('dap-go').debug_test()<CR>", desc = "Debug Nearest (Go)" },
+          },
+          settings = {
+            gopls = {
+              --gofumpt = false,
+              --codelenses = {
+              --  gc_details = false,
+              --  generate = true,
+              --  regenerate_cgo = true,
+              --  run_govulncheck = false,
+              --  test = false,
+              --  tidy = true,
+              --  upgrade_dependency = true,
+              --  vendor = true,
+              --},
+              ---- 这几个是干啥的
+              --hints = {
+              --  assignVariableTypes = true,
+              --  compositeLiteralFields = true,
+              --  compositeLiteralTypes = true,
+              --  constantValues = true,
+              --  functionTypeParameters = true,
+              --  parameterNames = true,
+              --  rangeVariableTypes = true,
+              --},
+              --analyses = {
+              --  fieldalignment = false,
+              --  nilness = true,
+              --  unusedwrite = true,
+              --  useany = true,
+
+              --  fillstruct = false, -- 关闭自动填充 struct. 默认打开
+              --  unusedparams = true,
+              --},
+
+              usePlaceholders = false, -- 填充补全后的 functions param. 默认打开
+              --completeUnimported = true,
+              --staticcheck = true,
+              --directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+              --semanticTokens = true,
+            },
+          },
+          flags = { allow_incremental_sync = true, debounce_text_changes = 150 },
+        },
+      },
     },
   },
 }
