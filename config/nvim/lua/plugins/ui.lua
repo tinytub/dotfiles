2a175b525bafe7f8584ac1a3d5e39fce8fd42cba
zhaopeng113 <zhaopeng18@jd.com>
2024-12-04 14:14:47 +0800

    update:

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -9,12 +9,12 @@ return {
         return
       end
 
-      -- HACK: #104 Invalid in command-line window
-      local hl = require("todo-comments.highlight")
-      local highlight_win = hl.highlight_win
-      hl.highlight_win = function(win, force)
-        pcall(highlight_win, win, force)
-      end
+      ---- HACK: #104 Invalid in command-line window
+      --local hl = require("todo-comments.highlight")
+      --local highlight_win = hl.highlight_win
+      --hl.highlight_win = function(win, force)
+      --  pcall(highlight_win, win, force)
+      --end
 
       todocomments.setup({
         --signs = false,
