a59f33856ab779a4bdf4ac4861702a920213faf6
zhaopeng113 <zhaopeng18@jd.com>
2025-10-26 23:31:34 +0800

    chore: lsp keys

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -1,13 +1,12 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    init = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- disable a keymap
-      keys[#keys + 1] = { "<c-k>", false }
-    end,
     opts = function(_, opts)
+      opts.servers = opts.servers or {}
+      opts.servers["*"] = opts.servers["*"] or {}
+      opts.servers["*"].keys = opts.servers["*"].keys or {}
+      opts.servers["*"].keys[#opts.servers["*"].keys + 1] = { "<c-k>", false }
+
       opts.inlay_hints = {
         enabled = false,
         exclude = { "vue" }, -- filetypes for which you don't want to enable inlay hints
